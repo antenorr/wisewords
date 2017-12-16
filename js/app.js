133dfa8e4c67b1736994c27c8cbb0d6c9cc28212
antenorr <roudyantenor@gmail.com>
2017-12-15 19:55:31 -0500

    complete 2/3rd of appleication by figuring out the AJAX / JSONP addition :)

--- js/app.js
+++ js/app.js
@@ -1,8 +1,50 @@
 'use strict';
 // hmmm it seems to just disable the button without toggleing the card as expected - i want once card is toggled THEN disable
 
-$("#quoteShower").on("click", function() {
-    $(this).prop("disabled", true)
-})
+// $("#quoteShower").on("click", function() {
+//     $(this).prop("disabled", true)
+// })
 
-console.log("chicken");
+
+
+
+
+
+$("button").on("click", function(e) {
+    console.log("chicken");
+    e.preventDefault();
+    var url = "http://quotes.stormconsultancy.co.uk/random.json";
+    var $content = $('.incoming');
+    var $contentAuthor = $('.incomingAuthor')
+
+
+
+    $.ajax({
+            type: "GET",
+            url: url,
+            beforeSend: function(){
+                $content.append('<div id="load">LOADING</div>');
+            },
+
+            complete: function() {
+                $('load').remove();
+            },
+            success: function(data) {
+                $content.html( $(data).find('#container') ).hide().fadeIn(400);
+                $content.text(data.quote); 
+                $contentAuthor.text(data.author);
+                // console.log(data.author);
+            },
+            error: function() {
+                $content.text('Please try again later!');
+            }
+            
+    });
+});
+
+
+
+
+
+
+
