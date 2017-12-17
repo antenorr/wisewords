8ceecd6fd4803c83c953f42b57748ae07c6586fb
antenorr <roudyantenor@gmail.com>
2017-12-17 03:30:16 -0500

    complete addition of the images to the quote car - mission NOT accomplished

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@
 
 
 $("button").on("click", function(e) {
-    console.log("chicken");
+    // console.log("chicken");
     e.preventDefault();
     var url = "http://quotes.stormconsultancy.co.uk/random.json";
     var $content = $('.incoming');
@@ -33,6 +33,25 @@ $("button").on("click", function(e) {
                 $content.html( $(data).find('#container') ).hide().fadeIn(400);
                 $content.text(data.quote); 
                 $contentAuthor.text(data.author);
+
+                    $.ajax({ 
+                        type: "GET",
+                        url: "https://api.unsplash.com/photos/?client_id=18dca1c976622fd5af0df12efba1de97d40aa22d8b508dd4b7a6ec24d7aea598",
+                        success: function(event) {
+                            console.log("chichichichic", event[0].urls["small"])
+                                 //   console.log(event[0].urls['small']);
+                                //  $('.background-img').css({"background-image" : "event[0].urls['small']" });
+                                //  $(".background-img").attr("src", "event[0].urls['small']");
+                               //  document.getElementById("background-img").src = "https://placekitten.com/g/700/300";
+                                 console.log("https://placekitten.com/g/700/300");
+                                  console.log(event[0].urls['small']);
+                                  
+
+
+
+                        }
+                        
+                    }) 
                 // console.log(data.author);
             },
             error: function() {
